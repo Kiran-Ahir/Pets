f252db91fcef74e35f6af7ce40a35b8864469030
Kiran-Ahir <kiranj695@gmail.com>
2021-07-20 13:23:16 +0530

    Final Change

--- app/src/main/java/com/example/pets/CatalogActivity.java
+++ app/src/main/java/com/example/pets/CatalogActivity.java
@@ -1,37 +1,39 @@
 package com.example.pets;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.loader.app.LoaderManager;
-import androidx.loader.content.CursorLoader;
-import androidx.loader.content.Loader;
-
+import android.content.ContentUris;
 import android.content.ContentValues;
 import android.content.Intent;
 import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
-import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.LoaderManager;
+import android.content.CursorLoader;
+import android.content.Loader;
+import android.widget.SimpleCursorAdapter;
 
 import com.example.pets.data.PetContract.PetEntry;
-import com.example.pets.data.PetDbHelper;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 /**
  * Displays list of pets that were entered and stored in the app.
  */
-public class CatalogActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor>, android.app.LoaderManager.LoaderCallbacks<D> {
+public class CatalogActivity extends AppCompatActivity implements
+        LoaderManager.LoaderCallbacks<Cursor> {
 
+    /** Identifier for the pet data loader */
     private static final int PET_LOADER = 0;
 
-    PetCursorAdapter mCursonAdapter;
+    /** Adapter for the ListView */
+    PetCursorAdapter mCursorAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,17 +50,36 @@ public class CatalogActivity extends AppCompatActivity implements LoaderManager.
             }
         });
 
-        // Find the Listview which will be populated with the pet data
+        // Find the ListView which will be populated with the pet data
         ListView petListView = (ListView) findViewById(R.id.list);
 
-        // Find and set EmptyView on the ListView, so that it only shows when the list has 0 item.
+        // Find and set empty view on the ListView, so that it only shows when the list has 0 items.
         View emptyView = findViewById(R.id.empty_view);
         petListView.setEmptyView(emptyView);
 
-        // Setup an adapter to create a list item for each row of pet data in the cursor
-        // There is no pet data yet (until the loader finishes) so pass in nul for the cursor
-        mCursonAdapter = new PetCursorAdapter(this, null);
-        petListView.setAdapter(mCursonAdapter);
+        // Setup an Adapter to create a list item for each row of pet data in the Cursor.
+        // There is no pet data yet (until the loader finishes) so pass in null for the Cursor.
+        mCursorAdapter = new PetCursorAdapter(this, null);
+        petListView.setAdapter(mCursorAdapter);
+
+        // Setup the item click listener
+        petListView.setOnItemClickListener((adapterView, view, position, id) -> {
+            // Create new intent to go to {@link EditorActivity}
+            Intent intent = new Intent(CatalogActivity.this, EditorActivity.class);
+
+            // Form the content URI that represents the specific pet that was clicked on,
+            // by appending the "id" (passed as input to this method) onto the
+            // {@link PetEntry#CONTENT_URI}.
+            // For example, the URI would be "content://com.example.android.pets/pets/2"
+            // if the pet with ID 2 was clicked on.
+            Uri currentPetUri = ContentUris.withAppendedId(PetEntry.CONTENT_URI, id);
+
+            // Set the URI on the data field of the intent
+            intent.setData(currentPetUri);
+
+            // Launch the {@link EditorActivity} to display the data for the current pet.
+            startActivity(intent);
+        });
 
         // Kick off the loader
         getLoaderManager().initLoader(PET_LOADER, null, this);
@@ -67,8 +88,7 @@ public class CatalogActivity extends AppCompatActivity implements LoaderManager.
     /**
      * Helper method to insert hardcoded pet data into the database. For debugging purposes only.
      */
-    private void insertPet(){
-
+    private void insertPet() {
         // Create a ContentValues object where column names are the keys,
         // and Toto's pet attributes are the values.
         ContentValues values = new ContentValues();
@@ -82,7 +102,14 @@ public class CatalogActivity extends AppCompatActivity implements LoaderManager.
         // into the pets database table.
         // Receive the new content URI that will allow us to access Toto's data in the future.
         Uri newUri = getContentResolver().insert(PetEntry.CONTENT_URI, values);
+    }
 
+    /**
+     * Helper method to delete all pets in the database.
+     */
+    private void deleteAllPets() {
+        int rowsDeleted = getContentResolver().delete(PetEntry.CONTENT_URI, null, null);
+        Log.v("CatalogActivity", rowsDeleted + " rows deleted from pet database");
     }
 
     @Override
@@ -103,41 +130,38 @@ public class CatalogActivity extends AppCompatActivity implements LoaderManager.
                 return true;
             // Respond to a click on the "Delete all entries" menu option
             case R.id.action_delete_all_entries:
-                // Do nothing for now
+                deleteAllPets();
                 return true;
         }
         return super.onOptionsItemSelected(item);
     }
 
-    @NonNull
-    @org.jetbrains.annotations.NotNull
     @Override
-    public Loader<Cursor> onCreateLoader(int id, @Nullable @org.jetbrains.annotations.Nullable Bundle args) {
-        // Define a projection that specifies the columns from the table we care about
+    public Loader<Cursor> onCreateLoader(int i, Bundle bundle) {
+        // Define a projection that specifies the columns from the table we care about.
         String[] projection = {
                 PetEntry._ID,
                 PetEntry.COLUMN_PET_NAME,
-                PetEntry.COLUMN_PET_BREED
-        };
+                PetEntry.COLUMN_PET_BREED };
 
         // This loader will execute the ContentProvider's query method on a background thread
         return new CursorLoader(this,   // Parent activity context
-                PetEntry.CONTENT_URI,          // Provider content uri to query
-                projection,                    // Columns to include in  the resulting cursor
-                null,
-                null,
-                null);
+                PetEntry.CONTENT_URI,   // Provider content URI to query
+                projection,             // Columns to include in the resulting Cursor
+                null,                   // No selection clause
+                null,                   // No selection arguments
+                null);                  // Default sort order
     }
 
     @Override
-    public void onLoadFinished(@NonNull @org.jetbrains.annotations.NotNull Loader<Cursor> loader, Cursor data) {
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
         // Update {@link PetCursorAdapter} with this new cursor containing updated pet data
-        mCursonAdapter.swapCursor(data);
+        mCursorAdapter.swapCursor(data);
     }
 
     @Override
-    public void onLoaderReset(@NonNull @org.jetbrains.annotations.NotNull Loader<Cursor> loader) {
+    public void onLoaderReset(Loader<Cursor> loader) {
         // Callback called when the data needs to be deleted
-        mCursonAdapter.swapCursor(null);
+        mCursorAdapter.swapCursor(null);
     }
 }
